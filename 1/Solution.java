a8d53ad511b27931555abe79453c6a6abfb84af9
unknown <likai91088@likai91088.hnisi.com.cn>
2016-08-16 15:18:15 +0800

    1

---
+++ 1/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int i,j;
+        for(i = 0; i < nums.length-1; i++){
+            for(j = i+1; j < nums.length; j++){
+                if(nums[i]+nums[j] == target)
+                   return new int[]{i,j};
+            }
+        }
+        return null;
+    }
+}
