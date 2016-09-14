04bf2f829f6951e4128d4111695a4a4e88bdd491
unknown <likai91088@likai91088.hnisi.com.cn>
2016-09-14 17:11:14 +0800

    21

--- 21/Solution.java
+++ 21/Solution.java
@@ -10,6 +10,38 @@ public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if(l1 == null) return l2;
         if(l2 == null) return l1;
-        
+        ListNode root = null;
+        ListNode result = null;
+        while(l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                if(root == null){
+                    root = new ListNode(l1.val);
+                    result = root;   
+                }else{
+                    result.next = new ListNode(l1.val);
+                    result = result.next;
+                }
+             l1 = l1.next;
+            }else{
+              if(root == null){
+                    root = new ListNode(l2.val);
+                    result = root;
+                }else{
+                    result.next = new ListNode(l2.val);
+                    result = result.next;
+                }
+                l2 = l2.next;  
+            }
+        }
+
+        if(l1 == null){
+            while(l2 != null){result.next = new ListNode(l2.val);l2 = l2.next; result = result.next;}
+        }
+
+        if(l2 == null){
+             while(l1 != null){result.next = new ListNode(l1.val);l1 = l1.next; result = result.next;}
+        }
+
+        return root;
     }
 }
